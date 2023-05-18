2e371fe17d9e23b50845d168b08dcb3abb5041e1
balh-x <74802349+balh-x@users.noreply.github.com>
2023-05-18 06:04:11 +0100

    Update homepage.js

--- src/page/homepage.js
+++ src/page/homepage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { DOMHelper } from 'rsuite';
 import { Layout } from 'antd';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Card, Row, Col } from 'antd';
 const { Meta } = Card;
 const { Content } = Layout;
@@ -11,37 +11,31 @@ const Homepage = () => {
     {
       title: 'Cleaning',
       color: '#f0f2f5',
-      link: '/cleaning',
       cover: 'https://cache.handy-client-assets.com/taxonomy-service/service_faucets_replacement.jpg',
     },
     {
       title: 'Babysitting',
       color: '#e6f7ff',
-      link: '/babysitting',
       cover: 'https://compote.slate.com/images/5f52a993-0e74-4b9c-bdd9-5f1c286ec073.png?crop=1180%2C842%2Cx0%2Cy0&width=1440',
     },
     {
       title: 'Pest Control',
       color: '#fffbe6',
-      link: '/pest-control',
       cover: 'https://cdn.cdnparenting.com/articles/2018/09/453277711-H-1024x700.webp',
     },
     {
       title: 'Plumbing',
       color: '#fce4d6',
-      link: '/plumbing',
       cover: 'https://img.freepik.com/premium-vector/friendly-smiling-cartoon-professional-man-plumber-character-standing-uniform-isolated-white-flat-pipe-repair-tools-working-accessories-bathroom-toilet-set_575670-2220.jpg?w=1480',
     },
     {
       title: '',
       color: '#f9f0f0',
-      link: '/electrical-repairs',
       cover: 'https://images01.nicepagecdn.com/page/94/41/website-template-preview-94417.webp',
     },
     {
       title: 'Beauty',
       color: '#fff0f6',
-      link: '/beauty',
       cover: 'https://www.odtap.com/wp-content/uploads/2019/03/beauty-banners.png',
     },
   ];
@@ -53,7 +47,7 @@ const Homepage = () => {
           <Row gutter={[25, 40]}>
             {cards.map((card) => (
               <Col span={8} key={card.title}>
-                <Link to={card.link}>
+               
                   <Card
                     hoverable
                     style={{
@@ -77,7 +71,7 @@ const Homepage = () => {
                       }}
                     />
                   </Card>
-                </Link>
+              
               </Col>
             ))}
           </Row>
